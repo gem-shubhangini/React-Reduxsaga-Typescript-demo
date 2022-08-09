608f09ba92fba5991ceebdfbbd9daf2f08550ec5
SHUBHANGINI SHARMA <SHUBHANGINISHARMA67@gmail.com>
2022-08-09 15:16:43 +0530

    commit

--- src/redux/reducer.tsx
+++ src/redux/reducer.tsx
@@ -1,11 +1,11 @@
 
 import { Add_To_Cart, EMPTY_Cart, REMOVE_From_Cart } from "./constant"
-import {Action} from './Interface'
+import {Action,Product} from './Interface'
 
 
    
 
-const cartData =(data=[],action:Action)=>{
+const cartData =(data=[] ,action:Action)=>{
     //if else statement
     /*if(action.type===Add_To_Cart){
     console.log("Add_To_Cart Condition",action)
@@ -26,8 +26,13 @@ const cartData =(data=[],action:Action)=>{
         case REMOVE_From_Cart:
             console.log("Remove_To_Cart Condition",action)
              //data.length=data.length-1 
-
-             return [...data]
+             if(data!=[]){
+             const remainingitem = data.filter((item:any)=>item.id!==action.data)
+             console.warn("remaining items",remainingitem)
+             return [...remainingitem]
+             }
+              
+             return [...data];
         case EMPTY_Cart:
             console.log("Empty cart",action)
              data =[] 
